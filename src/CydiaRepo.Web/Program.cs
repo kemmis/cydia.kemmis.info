2c5abe63ed5096f2c90ec7ece7c5671b266cb485
Rafe Kemmis <rafe@kemmis.info>
2018-10-29 15:27:33 -0400

    added application insights

--- src/CydiaRepo.Web/Program.cs
+++ src/CydiaRepo.Web/Program.cs
@@ -19,6 +19,7 @@ namespace CydiaRepo.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
