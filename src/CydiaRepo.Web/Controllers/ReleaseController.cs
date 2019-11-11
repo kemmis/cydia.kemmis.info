20febccdf5b7a40fb38b58ef40543a2081b2f924
Rafe Kemmis <rafe@kemmis.info>
2019-11-10 23:28:55 -0500

    removed CR from new line output

--- src/CydiaRepo.Web/Controllers/ReleaseController.cs
+++ src/CydiaRepo.Web/Controllers/ReleaseController.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +10,20 @@ namespace CydiaRepo.Web.Controllers
     public class RepoInfoController : Controller
     {
         [Route("/Release")]
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            var sb = new StringBuilder();
+
+            sb.Append("Origin: cydia.kemmis.info\n");
+            sb.Append("Label: cydia.kemmis.info\n");
+            sb.Append("Suite: stable\n");
+            sb.Append("Version: 1.2\n");
+            sb.Append("Codename: ios\n");
+            sb.Append("Architectures: iphoneos - arm\n");
+            sb.Append("Components: main\n");
+            sb.Append("Description: cydia.kemmis.info\n");
+
+            return sb.ToString();
         }
     }
 }
